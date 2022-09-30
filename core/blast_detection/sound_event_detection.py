77971feef7cb9df2994c5260751d4f31724c194f
imran2205 <imrankabir1996@gmail.com>
2022-09-30 10:50:06 +0600

    commit blast detection

--- core/blast_detection/sound_event_detection.py
+++ core/blast_detection/sound_event_detection.py
@@ -9,21 +9,24 @@ import sounddevice as sd
 from keras_yamnet import params
 from keras_yamnet.yamnet import YAMNet, class_names
 from keras_yamnet.preprocessing import preprocess_input
+from copy import deepcopy
 
 from plot import Plotter
 
 if __name__ == "__main__":
 
     ################### SETTINGS ###################
-    plt_classes = [0,132,420,494] # Speech, Music, Explosion, Silence
-    # plt_classes = [420]
+    # plt_classes = [0, 132, 420, 494] # Speech, Music, Explosion, Silence
+    plt_classes = [
+        494, 0, 132, 420, 421, 422, 423, 424, 425, 426, 427, 428, 429, 430
+    ]
     class_labels = True
     FORMAT = pyaudio.paFloat32
     CHANNELS = 1
     RATE = params.SAMPLE_RATE
-    WIN_SIZE_SEC = 0.075
+    WIN_SIZE_SEC = 0.475
     CHUNK = int(WIN_SIZE_SEC * RATE)
-    RECORD_SECONDS = 60
+    RECORD_SECONDS = 600
 
     print(sd.query_devices())
     MIC = None
@@ -32,6 +35,7 @@ if __name__ == "__main__":
     
     model = YAMNet(weights='C:/Users/admin/Desktop/desktop/gta/DistanceMeasure/core/blast_detection/keras_yamnet/yamnet.h5')
     yamnet_classes = class_names('C:/Users/admin/Desktop/desktop/gta/DistanceMeasure/core/blast_detection/keras_yamnet/yamnet_class_map.csv')
+    silent_data = np.load("/resources/room_silence.npy")
 
     #################### STREAM ####################
     audio = pyaudio.PyAudio()
@@ -62,15 +66,20 @@ if __name__ == "__main__":
         curr_size = data.shape[0]
         pad_data = data
         if curr_size < 96:
-            pad_data = np.zeros((96, 64))
-            for j in range(46, 52, curr_size):
+            # pad_data = np.zeros((96, 64))
+            pad_data = deepcopy(silent_data)
+            for j in range(10, 20, curr_size):
                 pad_data[j:j+curr_size, 0:64] = data
         # pad_data = data
-        prediction = model.predict(np.expand_dims(pad_data, 0))
+        prediction = model.predict(np.expand_dims(pad_data, 0), verbose=0)
         # print(prediction)
-        print(prediction[0][plt_classes])
+        # print(prediction[0][plt_classes])
+        print(
+            plt_classes_lab[np.argmax(prediction[0][plt_classes])],
+            np.max(prediction[0][plt_classes])
+        )
+        # np.save(f"{np.argmax(prediction[0][plt_classes])}_{int(prediction[0][plt_classes][np.argmax(prediction[0][plt_classes])]*100)}", pad_data)
         monitor(pad_data.transpose(), np.expand_dims(prediction[0][plt_classes], -1))
-        print(i)
 
     print("finished recording")
 
